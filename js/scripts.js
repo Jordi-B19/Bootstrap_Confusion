af9c9bc5c7d82abd00c872a3067d6ead37c15eab
Jordi-B19 <63915364+Jordi-B19@users.noreply.github.com>
2020-07-23 10:40:32 +0200

    build scripts

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,10 @@ $(document).ready(function(){
             $("#carouselButton").children("span").addClass('fa-pause');                    
         }
     });
-    $("#loginButton").click(()=>{
+    $("#loginButton").click(function(){
         $("#loginModal").modal("toggle")
     });
-    $("#reserveButton").click(()=>{
+    $("#reserveButton").click(function(){
         $("#reserveModal").modal("toggle")
     });
 });
